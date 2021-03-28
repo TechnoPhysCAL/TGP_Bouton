7111332410a91460c5f9876ea5dbb05d70f20b0f
Claude Bouchard <claudebouchard2@gmail.com>
2021-03-28 14:21:46 -0400

    Update Bouton.h

--- Bouton.h
+++ Bouton.h
@@ -1,13 +1,6 @@
 #ifndef Bouton_h
 #define Bouton_h
 
-#define RELACHE 0
-#define RELACHANT 1
-#define ENFONCE 2
-#define ENFONCANT 3
-#define MAINTENU 6
-#define MAINTENANT 7
-
 #include "BoutonBase.h"
 
 class Bouton : public BoutonBase
